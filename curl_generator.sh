1ba5f44cb3e50c0e0fb46f2681b8136e6fd2755f
Andre Oliveira <andre.oliveira-impulso@m4u.com.br>
2020-05-11 20:30:30 -0400

    (new feature) adding many headers to result of curl

--- curl_generator.sh
+++ curl_generator.sh
@@ -6,8 +6,7 @@ read -p 'Enter your url (without transfer protocol): ' url
 
 protocols=("http" "https")
 
-select protocol in "${protocols[@]}"
-do
+select protocol in "${protocols[@]}"; do
   case $protocol in
     "http" | "https")
       protocol="$protocol://"
@@ -19,8 +18,34 @@ done
 
 read -p 'Enter how many headers: ' n_headers
 
+header_options=("Bearer Authorization" "Authorization" "Content-Type: application/json" "Custom Header")
+headers=()
+
 for n in $(seq 1 $n_headers); do
-  echo "safe"
+  select selected_header in "${header_options[@]}"; do
+    case $selected_header in
+      "Bearer Authorization")
+        read -p "Enter the value of Bearer Token Authorization " header_value
+        headers+=" -H 'Authorization: Bearer $header_value'"
+        break
+        ;;
+      "Authorization")
+        read -p 'Enter the value of Token Authorization ' header_value
+        headers+=" -H 'Authorization: $header_value'"
+        break
+        ;;
+      "Content-Type: application/json")
+        headers+=" -H '$selected_header'"
+        break
+        ;;
+      "Custom Header")
+        read -p 'Enter the header and value (Custom: Header) ' header_value
+        headers+=" -H '$selected_header'"
+        break
+        ;;
+      *) echo "invalid option $REPLY";;
+    esac
+  done
 done
 
-echo "curl '$protocol$url' | jq"
+echo "curl $headers '$protocol$url' | jq"
